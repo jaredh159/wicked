2982c0962f0e5c87b3fce6cd0b3b13ca2c8d8e7e
Jared Henderson <jaredh159@users.noreply.github.com>
2023-12-29 12:40:55 -0500

    dedupe input domains into smaller file

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,30 @@
 
 mod check;
 mod db;
+mod dedupe;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+// 1. take file, dedupe into another file
+// 2. rework db reset to drop table, recreate
+// 3. add run --sample=100_000, w/ plan:
+//      - spawn tasks to:
+//      - take a random domain from file
+//      - if it's already been processed, go back a step
+//      - test for reachability
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() -> Result<(), Error> {
   dotenv::dotenv().ok();
-  let cmd = std::env::args().nth(1);
+  let cmd = std::env::args()
+    .nth(1)
+    .expect("missing required [cmd] arg (dedupe|db-reset)");
   let client = db::connect().await?;
 
-  if cmd == Some(String::from("db:reset")) {
-    db::reset(&client, db::Conf { limit: 414_865_650, offset: 0 }).await?;
+  match cmd.as_str() {
+    "dedupe" => dedupe::run()?,
+    "db-reset" => db::reset(&client, db::Conf { limit: 0, offset: 0 }).await?,
+    _ => panic!("unknown command: `{}`", cmd),
   }
-
-  // let domain = db::random_domain(&client).await?;
-  // check::domain(domain).await?;
-
   Ok(())
 }
